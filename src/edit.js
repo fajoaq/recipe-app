883644d2c92c1ca659121ce3a026a77839f95c93
Francis J <fajoaq@gmail.com>
2020-08-17 12:16:02 -0400

    Create edit.js/edit.html and re-configure webpack

---
+++ src/edit.js
@@ -0,0 +1,12 @@
+import { getRecipes, saveRecipeList, saveRecipe, deleteRecipe, createRecipe } from './recipe'
+import renderRecipePreview from './views'
+
+/* renderRecipePreview() */
+
+/* document.querySelector('#search-recipes').addEventListener('input', (e) => {
+    renderRecipePreview(e.target.value)
+})
+document.querySelector('button').addEventListener('click', (e) => {
+    createRecipe()
+    renderRecipePreview()
+}) */
